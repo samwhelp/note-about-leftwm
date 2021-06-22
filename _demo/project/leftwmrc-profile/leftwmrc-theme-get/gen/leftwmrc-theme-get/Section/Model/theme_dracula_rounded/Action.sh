b858058a0ad5fdd80fdb6ad689424a5c3320d6b5
samwhelp <samwhelp@users.noreply.github.com>
2021-06-23 01:45:59 +0800

    theme_dracula_rounded

---
+++ _demo/project/leftwmrc-profile/leftwmrc-theme-get/gen/leftwmrc-theme-get/Section/Model/theme_dracula_rounded/Action.sh
@@ -0,0 +1,6 @@
+
+
+sub_run_dracula_rounded () {
+	#echo "sub_run_dracula_rounded"
+	mod_dracula_rounded_install "$@"
+}
---
+++ _demo/project/leftwmrc-profile/leftwmrc-theme-get/gen/leftwmrc-theme-get/Section/Model/theme_dracula_rounded/Core.sh
@@ -0,0 +1,20 @@
+
+
+################################################################################
+### Head: Model dracula_rounded
+##
+
+mod_dracula_rounded_install () {
+
+	## https://github.com/AethanFoot/leftwm-theme-dracula-rounded
+
+	echo
+	leftwmrc_theme_repo_clone 'dracula_rounded' 'https://github.com/AethanFoot/leftwm-theme-dracula-rounded.git'
+	echo
+
+
+}
+
+##
+### Tail: Model dracula_rounded
+################################################################################
